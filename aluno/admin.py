10e77645107736bdf3c30a5f17cd78bf20ecd3a9
albertojr <alberto.crato13@gmail.com>
2021-02-12 18:11:10 -0300

    personalizaçao do aluno admin para busca, versionamento do js e css

--- aluno/admin.py
+++ aluno/admin.py
@@ -3,6 +3,8 @@ from .models import Aluno
 
 # Register your models here.
 class AlunoAdmin(admin.ModelAdmin):
+ 
+    list_display = ('cod_aluno', 'nome_aluno','dt_nascimento_aluno', 'filiacao_aluno1','filiacao_aluno2')
     search_fields = ('nome_aluno',)
 
 admin.site.register(Aluno,AlunoAdmin)
