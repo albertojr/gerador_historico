71da89af2519cc74b9c8974da5ead039756bd62a
albertojr <alberto.crato13@gmail.com>
2021-02-12 17:46:18 -0300

    tempo para expirar login

--- core/settings.py
+++ core/settings.py
@@ -122,6 +122,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+# tempo de sessão logado, definido 8hr de sessão
+SESSION_COOKIE_AGE = 14400
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
